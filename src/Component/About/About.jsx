540f874daec605f097c520cd9a4a8098d5ffcb05
MikeWeDev <155253264+MikeWeDev@users.noreply.github.com>
2024-03-14 15:37:23 -0300

    first part the enviroment

---
+++ src/Component/About/About.jsx
@@ -0,0 +1,10 @@
+import './About.css'
+function About() {
+  return (
+    <div>
+      about
+    </div>
+  )
+}
+
+export default About
---
+++ src/Component/Service/Service.jsx
@@ -0,0 +1,10 @@
+import './Service.css'
+function Service() {
+  return (
+    <div>
+      service
+    </div>
+  )
+}
+
+export default Service
