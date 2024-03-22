ee833da3d22b7aec0ee2381839b481e0c5c1b5b4
alazarr15 <147824564+alazarr15@users.noreply.github.com>
2024-03-22 21:13:19 +0300

    home page finalized

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,47 +1,64 @@
-import homeCarImage from "../images/home-car.png";
+import React, { useState } from 'react';
+import por from "../images/por.jpg";
+import logos from "../images/logos.png";
 import facebook from "../images/facebook.png";
 import insta from "../images/insta.png";
 import telegram from "../images/telegram.png";
-import logos from "../images/logos.png"
-import './Home.css'
+//import { GiHamburgerMenu } from "react-icons/gi";
+import "./Home.css";
+
 function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  
+
   return (
-  <> 
+    <>
+      <div className="homeImage">
+        <img src={por} alt="por" />
+      </div>
 
-<div className="home-container">
-  <div className="nav-container">
-    <div className="logo">
-      <img src={logos} alt="logo" />
-    </div>
-    <div className="nav-bar">
-      <p>Home</p>
-      <p>About</p>
-      <p>Product</p>
-      <p>Service</p>
-      <p>Contact</p>
-    </div>
-  </div>
-<div className="home-info">
-  <div className="home-description">Lorem ipsum dolor sit amet consectetur 
-   elit. Rerum, nihil.</div>
-  <div className="home-media">
-  <img src={facebook} alt="facebook" />
-  <img src={insta} alt="insta" />
-  <img src={telegram} alt="telegram" />
-  </div>
-</div>
-</div>
+      <nav>
 
-<div className="home-image-container">
-<img src={homeCarImage} alt="HomeCar" />
+      <div className="nav-container">
+        <div className="logo">
+          <img src={logos} alt="logo" />
+        </div>
+        <div className={`nav-bar${menuOpen ? " open" : ""}`}>
+          <p>Home</p>
+          <p>About</p>
+          <p>Product</p>
+          <p>Service</p>
+          <p>Contact</p>
+        </div>
+      </div>
+      
+      <div className={`menu ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+  <span></span>
+  <span></span>
+  <span></span>
 </div>
 
-<div className="home-nav-button">
-  <button>Call US</button>
-</div>
+      </nav>
+     
+      <div className="socialImage">
+        <img src={facebook} alt="facebook" />
+        <img src={insta} alt="insta" />
+        <img src={telegram} alt="telegram" />
+      </div>
 
-   </>
-   )
+      <div className="description">
+        <div className="dis-title">
+          Moges Motors
+        </div>
+        <div className="home-description">
+         <p> Rev up your ride with the ultimate car market experience!
+          <br></br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ea!</p>
+       </div>
+        <button>Contact Us!</button>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
